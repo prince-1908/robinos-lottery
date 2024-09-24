0ffa3adadbde4ff40da3ed7655e4d50f79930df5
Prince Singh Chouhan <princesinghchouhan7470@gmail.com>
2024-09-24 23:04:09 +0530

    new ui theme

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,11 +5,21 @@ import InputLabel from '@mui/material/InputLabel';
 import MenuItem from '@mui/material/MenuItem';
 import FormControl from '@mui/material/FormControl';
 import Select, { SelectChangeEvent } from '@mui/material/Select';
+import { Minus, Plus } from 'lucide-react';
+import Image from 'next/image';
 
 export default function Home() {
-  const [age, setAge] = useState('');
-  const [isWhitelisted, setIsWhitelisted] = useState(false);
+  const [age, setAge] = useState<string>('');
+  const [isWhitelisted, setIsWhitelisted] = useState<boolean>(false);
+  const [value, setValue] = useState<number>(3);
 
+  const handleReduce = () => {
+    setValue(value - 1);
+  }
+
+  const handleIncrease = () => {
+    setValue(value + 1);
+  };
   const handleChange = (event: SelectChangeEvent) => {
     setAge(event.target.value);
   };
@@ -18,137 +28,161 @@ export default function Home() {
     setIsWhitelisted(true);
   }
   return (
-    <div className="bg-image h-screen flex justify-center items-center">
-      <main className="w-1/2 gap-8 flex flex-col justify-between border p-8 rounded-2xl backdrop-blur-xl">
+    <div className="h-screen flex justify-center items-center">
+      <main className="w-full h-full container-shadow grid place-items-center">
 
-        <div className="flex justify-between">
-          <button className="border py-2 px-7 rounded-lg bg-white/10 hover:bg-white/15 transition">
-            Lottery
-          </button>
-
-          <div className="flex gap-4 items-center">
-            <p className="text-2xl">
-              Tickets
-            </p>
-            <div className="border p-2 rounded-lg bg-white/10 ">
-              0/10
-            </div>
-            <button className="border py-2 px-7 rounded-lg bg-white/10 hover:bg-white/15 transition">
-              Wallet
+        <div className='gap-8 flex flex-col justify-between py-8  w-full relative'>
+          <div className="flex justify-between px-72">
+            <button className="bg-blue-button rounded-lg px-10 py-[0.66rem] font-semibold text-2xl">
+              Lottery
             </button>
-          </div>
-        </div>
-
-        <div className="h-1/2">
-          <p className="text-3xl mb-2">Round #2</p>
-          <div className="border rounded-xl p-4 bg-white/5 gap-5 grid grid-cols-2 grid-rows-2">
-            <div className="grid gap-2 place-items-center">
-              <p className="text-xl">
-                Prize Pool
+            <div className="flex gap-4 items-center">
+              <p className="text-4xl font-semibold">
+                Tickets
               </p>
-              <button className="border rounded-lg py-4 w-full text-2xl bg-white/10 hover:bg-white/15 transition">
-                500 ICE
-              </button>
-            </div>
-
-            <div className="grid gap-2 place-items-center">
-              <p className="text-xl">
-                Deadline
-              </p>
-              <button className="border rounded-lg py-4 w-full text-2xl bg-white/10 hover:bg-white/15 transition">
-                20m:30s
-              </button>
-            </div>
-
-            <div className="grid gap-2 place-items-center">
-              <p className="text-xl">
-                Ticket
-              </p>
-              <button className="border rounded-lg py-4 w-full text-2xl bg-white/10 hover:bg-white/15 transition">
-                Sold: 7 <br /> Price 1 ICE
-              </button>
-            </div>
-
-            <div className="grid gap-2 place-items-center">
-              <p className="text-xl">
-                Last Win
-              </p>
-              <button className="border rounded-lg py-4 w-full text-2xl bg-white/10 hover:bg-white/15 transition">
-                Amount: 15.5 ICE <br /> User: 0x653...764
+              <div className="blue-invert rounded-lg px-10 py-[0.66rem] font-semibold text-2xl">
+                0
+              </div>
+              <button className="bg-blue-button rounded-lg px-10 py-[0.66rem] font-semibold text-2xl">
+                Wallet
               </button>
             </div>
           </div>
-        </div>
 
-        <div className="flex justify-evenly items-center">
-          <div className="flex gap-2 items-center justify-center">
-            <button className='text-4xl grid place-items-center'>
-              -
-            </button>
-            <div className="border rounded-md px-7 text-xl py-1">3</div>
-            <button className='text-4xl grid place-items-center'>
-              +
-            </button>
+          <div className='absolute z-[-1000] top-[75px] h-[70%] w-full'>
+            <Image
+              src="/igloo13.jpg"
+              alt=""
+              height={2160}
+              width={4100}
+              quality={100}
+              className='object-cover w-full h-full overflow-hidden'
+            />
+            <div className="absolute inset-x-0 bottom-0 h-[20%] bg-gradient-to-t from-black to-transparent"></div>
+            <div className="absolute inset-x-0 top-0 h-[20%] bg-gradient-to-b from-black to-transparent"></div>
           </div>
-          <div className='relative'>
-            <button className="border px-7 py-1 text-xl rounded-md">
-              3
-            </button>
-            <div className="absolute text-xs bottom-[-20px]">Balance: 456</div>
+
+          <div className="h-1/2 px-72">
+            <p className="text-5xl font-bold mb-2">Round #2</p>
+            <div className="border-t p-4  gap-5 grid grid-cols-2 grid-rows-2">
+              <div className="grid gap-2 place-items-center">
+                <p className="text-xl">
+                  Prize Pool
+                </p>
+                <button className="rounded-lg py-4 w-full text-2xl font-semibold bg-blue-button transition">
+                  500 ICE
+                </button>
+              </div>
+              <div className="grid gap-2 place-items-center">
+                <p className="text-xl">
+                  Deadline
+                </p>
+                <button className=" rounded-lg py-4 w-full text-2xl bg-blue-button font-semibold transition">
+                  20m:30s
+                </button>
+              </div>
+              <div className="grid gap-2 place-items-center">
+                <p className="text-xl">
+                  Ticket
+                </p>
+                <button className="rounded-lg py-4 w-full text-2xl bg-blue-button font-semibold transition">
+                  Sold: 7 <br /> Price 1 ICE
+                </button>
+              </div>
+              <div className="grid gap-2 place-items-center">
+                <p className="text-xl">
+                  Last Win
+                </p>
+                <button className="rounded-lg py-4 w-full text-2xl bg-blue-button font-semibold transition">
+                  Amount: 15.5 ICE <br /> User: 0x653...764
+                </button>
+              </div>
+            </div>
           </div>
-          <div>
-            <FormControl sx={{ m: 1, minWidth: 120 }} size="small">
-              <InputLabel
-                id="demo-select-small-label"
-                sx={{
-                  color: "white",
-                  '&.Mui-focused': {
-                    color: 'white',  // Color when the label floats (on focus)
-                  },
-                }}
-              >Age</InputLabel>
-              <Select
-                labelId="demo-select-small-label"
-                id="demo-select-small"
-                value={age}
-                label="Age"
-                onChange={handleChange}
-                sx={{
-                  color: 'white',
-                  '& .MuiOutlinedInput-notchedOutline': {
-                    borderColor: 'white',
-                  },
-                  '&:hover .MuiOutlinedInput-notchedOutline': {
-                    borderColor: 'white',
-                  },
-                  '&.Mui-focused .MuiOutlinedInput-notchedOutline': {
-                    borderColor: 'white',
-                  },
-                  '.MuiSelect-icon': {
-                    color: 'white',  // Arrow icon color
-                  },
-                }}
+          <div className="flex justify-evenly items-center px-72">
+            <div className="flex gap-2 items-center justify-center">
+              <div className=''>
+                <button
+                  className='text-4xl grid place-items-center'
+                  onClick={handleReduce}
+                >
+                  <Minus />
+                </button>
+              </div>
+              <div className="white-invert rounded-xl px-7 text-xl py-1">
+                {value}
+              </div>
+              <button
+                className='text-4xl grid place-items-center'
+                onClick={handleIncrease}
               >
-                <MenuItem value="">
-                  <em>None</em>
-                </MenuItem>
-                <MenuItem value={10}>Ten</MenuItem>
-                <MenuItem value={20}>Twenty</MenuItem>
-                <MenuItem value={30}>Thirty</MenuItem>
-              </Select>
-            </FormControl>
+                <Plus />
+              </button>
+            </div>
+            <div className='relative'>
+              <div className="white-invert px-7 py-1 text-xl rounded-xl">
+                3
+              </div>
+              <div className="absolute text-white text-xs bottom-[-20px]">Balance: 456</div>
+            </div>
+            <div>
+              <FormControl sx={{ m: 1, minWidth: 120 }} size="small">
+                <InputLabel
+                  id="demo-select-small-label"
+                  sx={{
+                    color: "white",
+                    '&.Mui-focused': {
+                      color: 'white',
+                    }
+                  }}
+                >Age</InputLabel>
+                <Select
+                  labelId="demo-select-small-label"
+                  id="demo-select-small"
+                  value={age}
+                  label="Age"
+                  onChange={handleChange}
+                  sx={{
+                    color: 'white',
+                    '& .MuiOutlinedInput-notchedOutline': {
+                      borderColor: 'white',
+                    },
+                    '&:hover .MuiOutlinedInput-notchedOutline': {
+                      borderColor: 'white',
+                    },
+                    '&.Mui-focused .MuiOutlinedInput-notchedOutline': {
+                      borderColor: 'white',
+                    },
+                    '.MuiSelect-icon': {
+                      color: 'white',
+                    },
+                  }}
+                >
+                  <MenuItem value="">
+                    <em>None</em>
+                  </MenuItem>
+                  <MenuItem value={10}>Ten</MenuItem>
+                  <MenuItem value={20}>Twenty</MenuItem>
+                  <MenuItem value={30}>Thirty</MenuItem>
+                </Select>
+              </FormControl>
+            </div>
           </div>
-        </div>
-
-        <div>
-          {!isWhitelisted ?
-            <button onClick={handleWhitelist} className='border w-full text-4xl py-4 rounded-xl font-bold bg-white/10 hover:bg-white/20 transition'>
-              Whitelist
-            </button>
-            : <button className='border w-full text-4xl py-4 rounded-xl font-bold bg-white/10 hover:bg-white/20 transition'>
-              Approve
-            </button>   
-          }
+          <div className='px-72'>
+            {!isWhitelisted ?
+              <button onClick={handleWhitelist} className='w-full text-4xl py-4 rounded-xl font-bold bg-blue-button transition'>
+                Whitelist
+              </button>
+              : <div className='flex justify-between'>
+                <button className='text-4xl py-4 rounded-xl font-bold bg-blue-button transition w-[49%]'>
+                  Approve
+                </button>
+                <button className='text-4xl py-4 rounded-xl font-bold bg-blue-button transition w-[49%]'>
+                  Buy
+                </button>
+              </div>
+            }
+          </div>
         </div>
       </main>
     </div>
