7cfed972cee2fc6e4e8db8e920c59de0a61371e7
Prince Singh Chouhan <princesinghchouhan7470@gmail.com>
2024-09-25 11:16:11 +0530

    ui responsive

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,11 +7,14 @@ import FormControl from '@mui/material/FormControl';
 import Select, { SelectChangeEvent } from '@mui/material/Select';
 import { Minus, Plus } from 'lucide-react';
 import Image from 'next/image';
+import { Nav } from '@/components/Nav';
+import { useMediaQuery } from '@mui/material';
 
 export default function Home() {
   const [age, setAge] = useState<string>('');
   const [isWhitelisted, setIsWhitelisted] = useState<boolean>(false);
   const [value, setValue] = useState<number>(3);
+  const isMobile = useMediaQuery('(max-width:768px)');
 
   const handleReduce = () => {
     setValue(value - 1);
@@ -28,26 +31,11 @@ export default function Home() {
     setIsWhitelisted(true);
   }
   return (
-    <div className="h-screen flex justify-center items-center">
+    <div className="md:h-screen flex justify-center items-center pb-16 md:pb-0">
       <main className="w-full h-full container-shadow grid place-items-center">
 
         <div className='gap-8 flex flex-col justify-between py-8  w-full relative'>
-          <div className="flex justify-between px-72">
-            <button className="bg-blue-button rounded-lg px-10 py-[0.66rem] font-semibold text-2xl">
-              Lottery
-            </button>
-            <div className="flex gap-4 items-center">
-              <p className="text-4xl font-semibold">
-                Tickets
-              </p>
-              <div className="blue-invert rounded-lg px-10 py-[0.66rem] font-semibold text-2xl">
-                0
-              </div>
-              <button className="bg-blue-button rounded-lg px-10 py-[0.66rem] font-semibold text-2xl">
-                Wallet
-              </button>
-            </div>
-          </div>
+          <Nav />
 
           <div className='absolute z-[-1000] top-[75px] h-[70%] w-full'>
             <Image
@@ -62,9 +50,9 @@ export default function Home() {
             <div className="absolute inset-x-0 top-0 h-[20%] bg-gradient-to-b from-black to-transparent"></div>
           </div>
 
-          <div className="h-1/2 px-72">
+          <div className="h-1/2 px-4 sm:px-8 md:px-16 lg:px-40 xl:px-72">
             <p className="text-5xl font-bold mb-2">Round #2</p>
-            <div className="border-t p-4  gap-5 grid grid-cols-2 grid-rows-2">
+            <div className="border-t pt-8 p-4  gap-8 md:gap-5 grid grid-cols-1 md:grid-cols-2 grid-rows-2">
               <div className="grid gap-2 place-items-center">
                 <p className="text-xl">
                   Prize Pool
@@ -99,8 +87,8 @@ export default function Home() {
               </div>
             </div>
           </div>
-          <div className="flex justify-evenly items-center px-72">
-            <div className="flex gap-2 items-center justify-center">
+          <div className="flex flex-col gap-8 md:gap-0 md:flex-row justify-evenly items-center px-8 sm:px-8 md:px-16 lg:px-40 xl:px-72">
+            <div className="flex gap-2 items-center w-full md:w-auto">
               <div className=''>
                 <button
                   className='text-4xl grid place-items-center'
@@ -109,7 +97,7 @@ export default function Home() {
                   <Minus />
                 </button>
               </div>
-              <div className="white-invert rounded-xl px-7 text-xl py-1">
+              <div className="white-invert rounded-xl px-7 text-xl py-1 grow md:grow-0 text-center">
                 {value}
               </div>
               <button
@@ -119,14 +107,21 @@ export default function Home() {
                 <Plus />
               </button>
             </div>
-            <div className='relative'>
-              <div className="white-invert px-7 py-1 text-xl rounded-xl">
+            <div className='relative w-full md:w-auto px-8 md:px-0'>
+              <div className="white-invert px-7 py-1 text-xl text-center rounded-xl">
                 3
               </div>
               <div className="absolute text-white text-xs bottom-[-20px]">Balance: 456</div>
             </div>
-            <div>
-              <FormControl sx={{ m: 1, minWidth: 120 }} size="small">
+            <div className='w-full md:w-auto px-8 md:px-0'>
+              <FormControl
+                sx={{
+                  m: 1,
+                  minWidth: isMobile ? "100%" : 120,
+                }}
+                size="small"
+                fullWidth={isMobile}
+              >
                 <InputLabel
                   id="demo-select-small-label"
                   sx={{
@@ -168,7 +163,7 @@ export default function Home() {
               </FormControl>
             </div>
           </div>
-          <div className='px-72'>
+          <div className='px-4 sm:px-8 md:px-16 lg:px-40 xl:px-72'>
             {!isWhitelisted ?
               <button onClick={handleWhitelist} className='w-full text-4xl py-4 rounded-xl font-bold bg-blue-button transition'>
                 Whitelist
